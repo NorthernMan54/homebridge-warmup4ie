8eb9ee22509ff97efb43571c95573fbcc726f1c0
NorthernMan54 <buyminivan-northern@yahoo.ca>
2019-06-21 23:51:38 -0400

    Day 6

--- index.js
+++ index.js
@@ -27,47 +27,41 @@
 'use strict';
 
 var debug = require('debug')('warmup4ie');
-var Service, Characteristic, UUIDGen, FakeGatoHistoryService, CustomCharacteristic, warmup4ie;
+var Service, Characteristic, UUIDGen, FakeGatoHistoryService, CustomCharacteristic;
 var os = require("os");
 var hostname = os.hostname();
+var Warmup4ie = require('./lib/warmup4ie.js').Warmup4IE;
 const moment = require('moment');
 
 var myAccessories = [];
-var session; // reuse the same login session
-var updating; // Only one change at a time!!!!
 var refresh, storage;
 
 module.exports = function(homebridge) {
-
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   UUIDGen = homebridge.hap.uuid;
   CustomCharacteristic = require('./lib/CustomCharacteristic.js')(homebridge);
   FakeGatoHistoryService = require('fakegato-history')(homebridge);
-  warmup4ie = require('./lib/warmup4ie.js').Warmup4IE;
 
   // warmup4ie.setCharacteristic(Characteristic);
 
   homebridge.registerPlatform("homebridge-warmup4ie", "warmup4ie", warmup4iePlatform);
-}
+};
 
 function warmup4iePlatform(log, config, api) {
-
   this.username = config['username'];
   this.password = config['password'];
-  refresh = config['refresh'] || 60; // Update every minute
+  this.refresh = config['refresh'] || 60; // Update every minute
   this.log = log;
   // this.devices = config['devices'];
   storage = config['storage'] || "fs";
-
-  updating = false;
 }
 
 warmup4iePlatform.prototype = {
   accessories: function(callback) {
     this.log("Logging into warmup4ie...");
     // debug("Rooms", this);
-    this.thermostat = new warmup4ie(this, function(err, rooms) {
+    this.thermostat = new Warmup4ie(this, function(err, rooms) {
       if (!err) {
         this.log("Found %s room(s)", rooms.length);
         rooms.forEach(function(room) {
@@ -82,14 +76,14 @@ warmup4iePlatform.prototype = {
       // pollDevices.call(this);
     }.bind(this));
 
-    setInterval(pollDevices.bind(this), 10000); // Poll every minute
+    setInterval(pollDevices.bind(this), this.refresh * 1000); // Poll every minute
   }
 };
 
 function pollDevices() {
   // debug("pollDevices", this.thermostat);
   this.thermostat.room.forEach(function(room) {
-    debug("Room", room);
+    // debug("Room", room);
     updateStatus(room);
   });
 }
@@ -97,7 +91,7 @@ function pollDevices() {
 function getAccessory(accessories, roomId) {
   var value;
   accessories.forEach(function(accessory) {
-    debug("Room", accessory.room.roomId, roomId);
+    // debug("Room", accessory.room.roomId, roomId);
     if (accessory.room.roomId === roomId) {
       value = accessory;
     }
@@ -108,8 +102,8 @@ function getAccessory(accessories, roomId) {
 function updateStatus(room) {
   debug("updateStatus %s", room.roomId);
   var acc = getAccessory(myAccessories, room.roomId);
-  debug("acc", acc);
-  var service = acc.getService(Service.Thermostat);
+  // debug("acc", acc);
+  var service = acc.thermostatService;
   service.getCharacteristic(Characteristic.TargetTemperature)
     .updateValue(Number(room.targetTemp / 10));
 
@@ -119,77 +113,33 @@ function updateStatus(room) {
   service.getCharacteristic(Characteristic.TargetHeatingCoolingState)
     .updateValue((room.runMode === "off" ? 0 : 1));
 
-  service = getAccessory(myAccessories, room.roomId).getService(Service.TemperatureService);
-  service.getCharacteristic(Characteristic.CurrentTemperature)
-    .updateValue(Number(this.room.airTemp / 10));
-}
-
-warmup4iePlatform.prototype.periodicUpdate = function(t) {
-  var t = updateValues(this);
-}
-
-function updateValues(that) {
-  that.log("updateValues", myAccessories.length);
-  myAccessories.forEach(function(accessory) {
-
-    session.CheckDataSession(accessory.deviceID, function(err, deviceData) {
-      if (err) {
-        that.log("ERROR: UpdateValues", accessory.name, err);
-        that.log("updateValues: Device not reachable", accessory.name);
-        //                accessory.newAccessory.updateReachability(false);
-        warmup4ie.login(that.username, that.password).then(function(login) {
-          that.log("Logged into warmup4ie!");
-          session = login;
-        }.bind(this)).fail(function(err) {
-          // tell me if login did not work!
-          that.log("Error during Login:", err);
-        });
-      } else {
-        //debug("Update Values", accessory.name, deviceData);
-        // Data is live
-
-        if (deviceData.deviceLive) {
-          //                    that.log("updateValues: Device reachable", accessory.name);
-          //                    accessory.newAccessory.updateReachability(true);
-        } else {
-          that.log("updateValues: Device not reachable", accessory.name);
-          //                    accessory.newAccessory.updateReachability(false);
-        }
-
-        that.log("Change", accessory.name, warmup4ie.diff(accessory.device, deviceData));
-        accessory.device = deviceData;
-
-        accessory.log_event_counter++;
-        if (!(accessory.log_event_counter % 10)) {
-          accessory.loggingService.addEntry({
-            time: moment().unix(),
-            currentTemp: roundInt(warmup4ie.toHBTemperature(deviceData, deviceData.latestData.uiData.DispTemperature)),
-            setTemp: roundInt(warmup4ie.toHBTargetTemperature(deviceData)),
-            valvePosition: roundInt(deviceData.latestData.uiData.EquipmentOutputStatus)
-          });
-          accessory.log_event_counter = 0;
-        }
-
-
-        updateStatus(accessory.thermostatService, deviceData);
-
-      }
+  acc.log_event_counter++;
+  if (!(acc.log_event_counter % 10)) {
+    acc.loggingService.addEntry({
+      time: moment().unix(),
+      currentTemp: service.getCharacteristic(Characteristic.CurrentTemperature).value,
+      setTemp: service.getCharacteristic(Characteristic.TargetTemperature).value,
+      valvePosition: service.getCharacteristic(Characteristic.TargetHeatingCoolingState).value
     });
-  });
+    acc.log_event_counter = 0;
+  }
+
+  service = acc.temperatureService;
+  service.getCharacteristic(Characteristic.CurrentTemperature)
+    .updateValue(Number(room.airTemp / 10));
 }
 
 // give this function all the parameters needed
 
 function Warmup4ieAccessory(that, name, room) {
-
-  var uuid = UUIDGen.generate(name);
-
   this.log = that.log;
   this.log("Adding warmup4ie Device", name);
   this.name = name;
   this.username = that.username;
   this.password = that.password;
   this.room = room;
+  this.log_event_counter = 0;
+  this.roomId = room.roomId;
 
   // this.thermostat = new warmup4ie(this, );
 }
@@ -224,69 +174,14 @@ Warmup4ieAccessory.prototype = {
     }
   },
 
+  */
   setTargetTemperature: function(value, callback) {
-    var that = this;
-    if (!updating) {
-      updating = true;
-
-      //    maxValue: 38,
-      //    minValue: 10,
-
-      that.log("Setting target temperature for", this.name, "to", value + "°");
-
-      if (value < 10)
-        value = 10;
-
-      if (value > 38)
-        value = 38;
-
-      value = warmup4ie.towarmup4ieTemperature(that, value);
-      // TODO:
-      // verify that the task did succeed
-
-      //            warmup4ie.login(this.username, this.password).then(function(session) {
-      var heatSetPoint, coolSetPoint = null;
-      switch (warmup4ie.toHomeBridgeHeatingCoolingSystem(that.device.latestData.uiData.SystemSwitchPosition)) {
-        case 0:
-          break;
-        case 1:
-          heatSetPoint = value;
-          break;
-        case 2:
-          coolSetPoint = value;
-          break;
-        case 3:
-          if (value < that.device.latestData.uiData.HeatSetpoint)
-            heatSetPoint = value;
-          else if (value > that.device.latestData.uiData.CoolSetpoint)
-            coolSetPoint = value;
-          else if ((that.device.latestData.uiData.HeatSetpoint - value) < (value - that.device.latestData.uiData.CoolSetpoint))
-            coolSetPoint = value;
-          else
-            heatSetPoint = value;
-          break;
-        default:
-          break;
-      }
-      that.log("setHeatCoolSetpoint", that.name, that.device.latestData.uiData.StatusHeat, that.device.latestData.uiData.StatusCool);
-      session.setHeatCoolSetpoint(that.deviceID, heatSetPoint, coolSetPoint, that.usePermanentHolds).then(function(taskId) {
-        that.log("Successfully changed temperature!", that.name, taskId);
-        if (taskId.success) {
-          that.log("Successfully changed temperature!", taskId);
-          callback();
-        } else {
-          that.log("Error: Unsuccessfully changed temperature!", that.name, taskId);
-          callback(new Error("Error: setHeatCoolSetpoint"));
-        }
-        updateValues(that); // refresh
-      }.bind(this)).fail(function(err) {
-        that.log('Error: setHeatCoolSetpoint', that.name, err);
-        callback(err);
-      });
-      updating = false;
-    }
+    this.log("Setting target temperature for", this.name, "to", value + "°");
+    debug("This", this);
+    this.thermostat.setTargetTemperature(this.roomId, value, callback);
   },
 
+  /*
   setCoolingThresholdTemperature: function(value, callback) {
     var that = this;
     if (!updating) {
@@ -334,7 +229,6 @@ Warmup4ieAccessory.prototype = {
       //    minValue: 10,
 
       that.log("Setting heating threshold temperature for", this.name, "to", value + "°");
-
 
       if (value < 10)
         value = 10;
@@ -405,13 +299,13 @@ Warmup4ieAccessory.prototype = {
         validValues: [0, 1]
       });
 
-      /*
-      .on('set', this.setTargetHeatingCooling.bind(this));
+//    this.thermostatService
+//      .getCharacteristic(Characteristic.TargetHeatingCoolingState)
+//      .on('set', this.setTargetHeatingCooling.bind(this));
 
     this.thermostatService
       .getCharacteristic(Characteristic.TargetTemperature)
       .on('set', this.setTargetTemperature.bind(this));
-    */
 
     this.thermostatService
       .getCharacteristic(Characteristic.TargetTemperature)
@@ -452,7 +346,3 @@ Warmup4ieAccessory.prototype = {
     return [informationService, this.thermostatService, this.temperatureService, this.loggingService];
   }
 };
-
-function roundInt(string) {
-  return Math.round(parseFloat(string) * 10) / 10;
-}
